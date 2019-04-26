4c00c700b5fb9b834b58fdb3f3c3f5cd94b1c7d5
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-04-26 17:06:39 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -11,11 +11,14 @@ def posts
 
 end
 
-def add_posts(post)
+def add_post(post)
+  post.author = self
 
 end
 
-def add_post_by_title
+def add_post_by_title(posttitle)
+  post = Post.new(posttitle)
+  post.author = self
 end
 
 def self.post_count
