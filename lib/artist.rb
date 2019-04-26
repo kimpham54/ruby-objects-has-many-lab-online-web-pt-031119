8234c29ad4779e5e0c1f6c1dbacb0461d0376e6e
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-04-26 16:46:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,7 @@
 class Artist
 
 attr_accessor :name
+@@allsongs
 
 def initialize(name)
   @songs = []
@@ -13,6 +14,7 @@ end
 
 def add_song(song)
   @songs << song
+  @@allsongs << song
   song.artist = self
 end
 
@@ -20,6 +22,7 @@ end
 def add_song_by_name(songname)
   newsong = Song.new(songname)
   @songs << newsong
+  @@allsongs << newsong
   newsong.artist = self
 end
 
